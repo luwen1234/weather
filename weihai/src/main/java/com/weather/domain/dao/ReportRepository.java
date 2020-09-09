4c8e6a17bb03130fa0680923799c791e8a6f4697
luwen <lu.wen@fulcruminfo.cn>
2020-09-09 20:00:10 +0800

    report 修改

---
+++ weihai/src/main/java/com/weather/domain/dao/ReportRepository.java
@@ -0,0 +1,11 @@
+package com.weather.domain.dao;
+
+import com.weather.domain.entity.Report;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReportRepository extends JpaRepository<Report, Long> {
+
+
+}
